f1b0d9fa4b739b48241ee0beac25bbc1296dde86
Sushant Ubale <sushantubale@gmail.com>
2019-01-08 22:41:03 -0600

    added collection view

---
+++ Cells/ChatMessageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChatMessageCell.swift
+//  Simple Chat
+//
+//  Created by Sushant Ubale on 1/8/19.
+//  Copyright © 2019 Sushant Ubale. All rights reserved.
+//
+
+import UIKit
+
+class ChatMessageCell: UICollectionViewCell {
+    
+}
